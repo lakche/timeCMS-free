77578d16b75643f9cc1102bc5fb871bfce878d0f
lakche <tdmdsay@tom.com>
2016-03-11 17:28:50 +0800

    后台统计页面增加缓存 过滤非公开模板

--- app/Http/Controllers/Admin/DashController.php
+++ app/Http/Controllers/Admin/DashController.php
@@ -14,13 +14,10 @@ class DashController extends Controller
     public function index()
     {
         $article_num = Article::count();
-        $articles = Article::sortByDesc('id')->take(5)->get();
         $user_num = User::count();
-        $users = User::sortByDesc('id')->take(5)->get();
         $person_num = Person::count();
-        $persons = Person::sortByDesc('id')->take(5)->get();
         $project_num = Project::count();
-        $projects = Project::sortByDesc('id')->take(5)->get();
-        return Theme::view('admin.dash.index',compact(['article_num','user_num','person_num','project_num','articles','users','persons','projects']));
+        $users = User::sortByDesc('id')->take(5)->get();
+        return Theme::view('admin.dash.index',compact('article_num','user_num','person_num','project_num','users'));
     }
 }
