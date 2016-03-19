2b63a8b00b2cd5246e21f0bc886a5a4bc70da80e
lakche <tdmdsay@tom.com>
2016-03-19 15:30:03 +0800

    修正首页样式

--- resources/views/time/welcome/index.blade.php
+++ resources/views/time/welcome/index.blade.php
@@ -55,7 +55,7 @@
                         </div>
                     </div>
                 @endforeach
-                    <div class="col-sm-12 col-md-12" id="person">
+                    <div class="col-sm-12 col-md-12" id="persons">
                         <div class="panel panel-primary">
                             <div class="panel-heading">荣誉殿堂
                                 <span class="pull-right"><a href="{{ url('person') }}">更多>></a></span>
