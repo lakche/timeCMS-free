c96d71e0af6e6b59246c62ef60b402bf3dca6407
lakche <tdmdsay@tom.com>
2017-01-27 22:52:36 +0800

    修正未定义变量的BUG，增加了logo

--- resources/views/time/layouts/_footer.blade.php
+++ resources/views/time/layouts/_footer.blade.php
@@ -3,7 +3,10 @@
         <p><a href="{{ url('/') }}">{{ $system['title'] }}</a> 是在timeCMS(时光CMS)基础上搭建起来的站点，也是timeCMS的官方站点</p>
         <p>
             {{ $system['copyright'] or '' }} {{ $system['record'] or '' }}
+            @if(isset($system['miitbeian']))
             <a href="http://www.miitbeian.gov.cn/" target="_blank">{{ $system['miitbeian'] or '' }}</a>
+            @endif
+            @if(isset($system['beian']))
             @if (preg_match('|(\d+)|',$system['beian'],$beian))
             <div style="width:300px;margin:0 auto; padding:20px 0;">
                 <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode={{ $beian[0] }}" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
@@ -11,6 +14,7 @@
                     <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">{{ $system['beian'] or '' }}</p>
                 </a>
             </div>
+            @endif
             @endif
         </p>
         <ul class="bs-docs-footer-links text-muted">
